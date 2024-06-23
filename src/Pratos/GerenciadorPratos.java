7d18cbe8a7141052c02d53e19cf4792dae773a4e
DaviCardosoF <cardosodaviferreira@gmail.com>
2024-06-23 17:19:00 -0300

    corrigindo bug

--- src/Pratos/GerenciadorPratos.java
+++ src/Pratos/GerenciadorPratos.java
@@ -1,5 +1,58 @@
 package Pratos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenciadorPratos {
-    
+    private List<Prato> pratos;
+    private int proximoId;
+
+    public GerenciadorPratos() {
+        this.pratos = new ArrayList<>();
+        this.proximoId = 1;
+    }
+
+    public void adicionarPrato(String nome, String descricao) {
+        Prato novoPrato = new Prato(proximoId, nome, descricao);
+        pratos.add(novoPrato);
+        proximoId++;
+    }
+
+    public Prato buscarPrato(int id) {
+        for (Prato prato : pratos) {
+            if (prato.getId() == id) {
+                return prato;
+            }
+        }
+        return null;
+    }
+
+    public void editarPrato(int id, String novoNome, String novaDescricao) {
+        Prato prato = buscarPrato(id);
+        if (prato != null) {
+            prato.setNome(novoNome);
+            prato.setDescricao(novaDescricao);
+        } else {
+            System.out.println("Prato não encontrado.");
+        }
+    }
+
+    public void removerPrato(int id) {
+        Prato prato = buscarPrato(id);
+        if (prato != null) {
+            pratos.remove(prato);
+        } else {
+            System.out.println("Prato não encontrado.");
+        }
+    }
+
+    public void mostrarPratos() {
+        if (pratos.isEmpty()) {
+            System.out.println("Nenhum prato cadastrado.");
+        } else {
+            for (Prato prato : pratos) {
+                System.out.println(prato);
+            }
+        }
+    }
 }
--- src/Pratos/Prato.java
+++ src/Pratos/Prato.java
@@ -1,5 +1,42 @@
 package Pratos;
 
 public class Prato {
-    
+    private int id;
+    private String nome;
+    private String descricao;
+
+    public Prato(int id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Prato{" +
+                "  id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
 }
