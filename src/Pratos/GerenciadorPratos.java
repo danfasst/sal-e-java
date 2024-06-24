45b49130ea5e03667fbde46c896dc6053019930e
danfasst <danielcfcunha2005@gmail.com>
2024-06-24 11:58:08 -0300

    bugs sistema

--- src/Pratos/GerenciadorPratos.java
+++ src/Pratos/GerenciadorPratos.java
@@ -2,47 +2,12 @@ package Pratos;
 
 import java.util.ArrayList;
 
-public class GerenciadorPratos {
-    private static ArrayList<Prato> pratos;
-
-    public GerenciadorPratos() {
-        pratos = new ArrayList<>();
-    }
+public abstract class GerenciadorPratos {
+    
+    private static ArrayList<Prato> pratos = new ArrayList<>();
 
     public static void adicionarPrato(Prato prato) {
         pratos.add(prato);
-    }
-
-    public static Prato buscarPrato(int id) throws Exception {
-
-        for (Prato prato : pratos) {
-            if (prato.getId() == id) {
-                return prato;
-            }
-        }
-
-        throw new Exception("\nPrato não encontrado!");
-    }
-
-    public static void editarPrato(int id, String novoNome, String novaDescricao) throws Exception {
-
-        Prato prato = buscarPrato(id);
-
-        if (prato != null) {
-            prato.setNome(novoNome);
-            prato.setDescricao(novaDescricao);
-        }
-
-        throw new Exception("\nPrato não encontrado!");
-    }
-
-    public static void removerPrato(int id) throws Exception {
-        Prato prato = buscarPrato(id);
-        if (prato != null) {
-            pratos.remove(prato);
-        }
-
-        throw new Exception("\nPrato não encontrado!");
     }
 
     public static void mostrarPratos() throws Exception {
